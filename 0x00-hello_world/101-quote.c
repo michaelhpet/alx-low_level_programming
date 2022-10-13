1adc74bdbcf0646e612f64cebf26520025f710c9
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-13 15:17:58 +0100

    no report

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - entry function
+ *
+ * prints to standard error
+ *
+ * Return: 1 (error)
+ *
+*/
+
+int main(void)
+{
+	char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
+	return (1);
+}
