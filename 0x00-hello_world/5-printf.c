d74fca2da1e84be30c8aec33766439942359d8dc
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-13 14:05:37 +0100

    wrote 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * prints a string
+ *
+ * Return: 0 (always)
+ *
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
