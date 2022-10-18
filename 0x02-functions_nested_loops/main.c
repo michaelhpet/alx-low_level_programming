f3fa4707ad0b179cadb13fc575902e401c869379
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-18 12:03:51 +0100

    wrote main.h

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry point of program
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,5 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_alphabet(void);
