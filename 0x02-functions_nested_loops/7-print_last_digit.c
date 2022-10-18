fcd12f8a83e610ddffa4d80296be299798c3d674
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-18 13:31:14 +0100

    fixed code style (betty)

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,10 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	
+
 	last_digit = last_digit >= 0 ? last_digit : (last_digit * -1);
 
 	_putchar(last_digit + 48);
-	
+
 	return (last_digit);
 }
