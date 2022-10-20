d8bcd5ee8926dc3d60f532295575eca29dc94a27
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-20 18:44:20 +0100

    fixed betty style

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -7,23 +7,35 @@
  */
 int main(void)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(100);
-    _putchar('\n');
-    print_number(1000);
-    _putchar('\n');
-    print_number(10);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    print_number(-618971910);
-    _putchar('\n');
-    print_number(-1);
-    _putchar('\n');
-    print_number(91821);
-    _putchar('\n');
-    print_number(115428);
-    _putchar('\n');
-    return (0);
+	print_number(-1000000);
+	_putchar(10);
+	print_number(-349028);
+	_putchar(10);
+	print_number(-87624);
+	_putchar(10);
+	print_number(-6183);
+	_putchar(10);
+	print_number(-826);
+	_putchar(10);
+	print_number(-10);
+	_putchar(10);
+	print_number(0);
+	_putchar(10);
+	print_number(24);
+	_putchar(10);
+	print_number(100);
+	_putchar(10);
+	print_number(4215);
+	_putchar(10);
+	print_number(1000);
+	_putchar(10);
+	print_number(45021);
+	_putchar(10);
+	print_number(100000);
+	_putchar(10);
+	print_number(8210182);
+	_putchar(10);
+
+
+	return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,6 @@
 void print_number(int n)
 {
 	int i, digits;
-	float n_tmp;
 
 	if (n < 0)
 	{
@@ -16,17 +15,16 @@ void print_number(int n)
 		n *= -1;
 	}
 
-	n_tmp = n;
-	digits = count_digits(n_tmp);
+	digits = count_digits(n);
 
 	if (n < 10)
 		_putchar(n + 48);
-	else if (n >= 10 && n < 100)
+	else if (n > 9 && n < 100)
 	{
 		_putchar((n / 10) + 48);
 		_putchar((n % 10) + 48);
 	}
-	else if (n >= 100 && n < 1000)
+	else if (n > 99 && n < 1000)
 	{
 		_putchar((n / 100) + 48);
 		_putchar(((n / 10) % 10) + 48);
@@ -51,7 +49,7 @@ int count_digits(int number)
 	int digits;
 
 	double n = number;
-	unsigned n_tmp = n;
+	unsigned int n_tmp = n;
 
 	digits = (n > 9) ? 2 : 1;
 
