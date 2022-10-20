62559912060b704469a03ce607e08b57a0dea70c
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-20 12:39:56 +0100

    fixed prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
  * main - prints prime factor of 612852475143
@@ -7,7 +8,18 @@
 
 int main(void)
 {
-	printf("%lu\n", 50829599);
+	unsigned long i, n, n_root;
+
+	n = 612852475143;
+	n_root = sqrt(n);
+
+	for (i = 3; i < n_root; i++)
+	{
+		while((n % i == 0) && n != i)
+			n /= i;
+	}
+
+	printf("%lu\n", n);
 
 	return (0);
 }
