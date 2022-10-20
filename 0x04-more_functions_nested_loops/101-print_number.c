8ceeb322ff004acf810d4c719d78dddf0a30dead
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-20 14:34:09 +0100

    first trial - print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,10 +10,43 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-		_putchar((n * - 1) + 48);
+		n *= -1;
 	}
-	else if (n < 10)
+	if (n < 10)
 		_putchar(n + 48);
-	else
-		_putchar(n);
+	else if (n >= 10 && n < 100)
+	{
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar((n / 100) + 48);
+		_putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 1000 && n < 10000)
+	{
+		_putchar((n / 1000) + 48);
+		_putchar(((n / 100) % 10) + 48);
+		_putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 10000 && n < 100000)
+	{
+		_putchar((n / 10000) + 48);
+		_putchar(((n / 1000) % 10) + 48);
+		_putchar(((n / 100) % 10) + 48);
+		_putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 100000 && n < 1000000)
+	{
+		_putchar((n / 100000) + 48);
+		_putchar(((n / 10000) % 10) + 48);
+		_putchar(((n / 1000) % 10) + 48);
+		_putchar(((n / 100) % 10) + 48);
+		_putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
 }
