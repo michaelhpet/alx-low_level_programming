75f27b9e2a58b91c56b92b34f92163c36d9df089
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-24 14:55:52 +0100

    added _strcpy() prototype

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,9 +8,9 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void _strcpy(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
