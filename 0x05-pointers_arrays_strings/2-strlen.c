0de67fb713c08104160c2df3b04c7add4e2545d5
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-24 10:19:04 +0100

    wrote print_rev()

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,9 +6,10 @@
 
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 
-	for (length = 0; s[length] != '\0'; length++);
+	while (s[length] != '\0')
+		length++
 
-	return length;
+	return (length);
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = _strlen(s);
+
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar(10);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string which length to compute
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
