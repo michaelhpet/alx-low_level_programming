b418e4e6bbdb0200c97598a1efcf120af5ec9e30
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-26 21:24:36 +0100

    _strcmp() first trial

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -8,10 +9,11 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+    char s1[] = "Okay";
+    char s2[] = "Okay!";
 
-    printf("%d\n", _strcmp(s1, s2));
+    printf("_strcmp - %d\n", _strcmp(s1, s2));
+    printf("strcmp - %d\n", strcmp(s1, s2));
     printf("%d\n", _strcmp(s2, s1));
     printf("%d\n", _strcmp(s1, s1));
     return (0);
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	unsigned int diff;
+	int i, diff;
 
 	diff = 0;
 	for (i = 0; (s1[i] != '\0' || s2[i] != '\0'); i++)
 	{
-		diff += s1[i] - s2[i];
-		printf("diff now %u\n", diff);
+		diff = s1[i] - s2[i];
+		if (diff != 0)
+			break;
 	}
+
 
 	return (diff);
 }
