decda15b1a013af73ea867e6d65bb8055ed40bc5
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-26 23:14:44 +0100

    no report

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,16 +6,12 @@
 
 char *cap_string(char *s)
 {
-	int i, j, s_len;
+	int i, j;
 
 	int separators[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 	int separators_len = 13;
 
-	s_len = 0;
-	while (s[s_len] != '\0')
-		s_len++;
-
-	for (i = 0; i < s_len; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < separators_len; j++)
 		{
@@ -24,9 +20,6 @@ char *cap_string(char *s)
 				if (s[i + 1] > 97 && s[i + 1] < 123)
 					s[i + 1] -= 32;
 			}
-
-			if (s[i] == 9)
-				s[i] = 32;
 		}
 	}
 
