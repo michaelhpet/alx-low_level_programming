60f02d8560095d6f96860860e70cba2bc4467a7c
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-27 16:00:13 +0100

    added prototype for print_buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,5 +18,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int char_len(char *s);
 char *prepend_zero(char *s, char *dest, int s_len, int size);
 char *trim_zero(char *s);
+void print_buffer(char *b, int size);
 
 #endif
