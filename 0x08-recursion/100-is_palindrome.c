86a82c45c41f7a65089cf2c94aa9f11fc673da45
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-02 22:34:35 +0100

    wrote is_palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks for palindrome string
+ * @s: string
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int len_s = _strlen(char *s);
+
+	if (*s == 0)
+		return (1);
+
+	return palindrome(s, s_len);
+}
+
+/**
+ * palindrome - checks for a palindrome string
+ * @s: string to check
+ * @s_len: length of s
+ * Return: 1 if s is palindrome, 0 if not
+ */
+
+int palindrome(char *s, int s_len)
+{
+	if (*(s + 1) == 0)
+		return (1);
+
+	if (*s != *(s + s_len))
+		return (0);
+
+	return (palindrome(s + 1, s_len - 1));
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string which length to compute
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	if (*s == 0)
+		return (0);
+
+	if (*(s + 1) == 0)
+		return (1);
+
+	return (_strlen(s + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,8 @@ int _sqrt_recursion(int n);
 int root(int n, int guess);
 int is_prime_number(int n);
 int prime(int n, int denom);
+int palindrome(char *s, int s_len);
 int is_palindrome(char *s);
+int _strlen(char *s);
 
 #endif
