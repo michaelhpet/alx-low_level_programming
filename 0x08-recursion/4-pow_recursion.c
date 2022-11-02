6a77d733e84ce5d555c96841b65523b27818e5ca
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-02 17:35:28 +0100

    wrote _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _pow_recursion - returns the value of a raised to the power of y
+ * @x: base
+ * @y: exponent
+ * Return: x exponent y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(y - 1));
+}
