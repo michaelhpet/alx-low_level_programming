1804608aaa5b831caaa9d270c6395e9aeac4076b
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-02 17:29:09 +0100

    wrote factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number
+ * Return: n! (n factorial)
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
