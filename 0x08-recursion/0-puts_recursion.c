601ca84b86989c865679ac142889075b135f6c2f
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-02 16:18:09 +0100

    wrote _puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+		return;
+
+	_putchar(*s);
+	_putchar(*(s + 1));
+}
