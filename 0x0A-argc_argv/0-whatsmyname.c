ad42cc8a492ddd3bfa5289b48095e0402991bbd9
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-04 09:24:58 +0100

    wrote whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a program name
+ * Return: 0 (success)
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
