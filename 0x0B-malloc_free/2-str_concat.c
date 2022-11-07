b2e12aecd87057ff8f360cf8320e50d5f406f8de
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-07 14:57:06 +0100

    refactor: str_concat uses _strlen

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -9,10 +10,11 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	int i, j, s1_len, s2_len;
 	char *str;
 
-	str = malloc(sizeof(s1) + sizeof(s2) - 1);
+	s1_len = _strlen(s1), s2_len = _strlen(s2);
+	str = malloc(sizeof(s1_len) + sizeof(s2_len) + 1);
 
 	if (str == NULL)
 		return (NULL);
@@ -27,4 +29,21 @@ char *str_concat(char *s1, char *s2)
 	str[i + j] = s2[i];
 
 	return (str);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+
+	return (i);
 }
