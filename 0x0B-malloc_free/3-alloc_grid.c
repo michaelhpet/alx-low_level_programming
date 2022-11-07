224c9b1c6e39b42ac3f7487d27dab2f06e8225d5
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-07 15:24:31 +0100

    no report

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(sizeof(int) * height);
+	array = malloc(sizeof(row) * height);
 
 	if (array == NULL)
 	{
