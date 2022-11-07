69a325ffec969ce5f8110a1e4753c6617d64d5f1
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-07 14:49:06 +0100

    refactor: str_copy uses strlen

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,14 @@
 
 char *_strdup(char *str)
 {
-	int i;
+	int i, str_len;
 	char *str_copy;
 
 	if (str == NULL)
 		return (NULL);
 
-	str_copy = malloc(sizeof(str));
+	str_len = _strlen(str);
+	str_copy = malloc(sizeof(char) * (strlen + 1));
 
 	if (str_copy == NULL)
 		return (NULL);
@@ -26,4 +27,21 @@ char *_strdup(char *str)
 	str_copy[i] = str[i];
 
 	return (str_copy);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+
+	return (i);
 }
