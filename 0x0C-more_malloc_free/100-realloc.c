4e4838e9b6b07ca9dbb60e9a4bb49fc3378f34db
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-10 17:19:58 +0100

    no report

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		old_size = 0;
 
-	mem = malloc(old_size + (new_size - old_size));
+	mem = malloc(sizeof(*(char *)ptr) * new_size);
 	if (mem == NULL)
 	{
 		free(ptr);
