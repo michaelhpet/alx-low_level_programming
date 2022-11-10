5f89f353829db505e1f15b54ff72452946166996
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-10 14:13:41 +0100

    wrote string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: first n bytes of s2 to copy
+ * Return: pointer to new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *string;
+	unsigned int i, j, total_len, s1_len, s2_len;
+
+	s1_len = _strlen(s1), s2_len = _strlen(s2);
+	if (n >= s2_len)
+		n = s2_len;
+
+	total_len = s1_len + n + 1;
+	string = malloc(sizeof(*string) * total_len);
+
+	if (string == NULL)
+		return (NULL);
+
+	for (i = 0; i < s1[i] != 0; i++)
+		string[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		string[j + i] = s2[j];
+
+	string[j + i] = 0;
+
+	return (string);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string
+ * Return: length of s
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int length;
+
+	length = 0;
+	while (s[length] != 0)
+		length++;
+
+	return (length);
+}
