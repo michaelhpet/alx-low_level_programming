c005f6f701c104536451a8cfa5dfc48d248385d4
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-10 14:16:35 +0100

    chore: cleanup

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -23,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (string == NULL)
 		return (NULL);
 
-	for (i = 0; i < s1[i] != 0; i++)
+	for (i = 0; s1[i] != 0; i++)
 		string[i] = s1[i];
 
 	for (j = 0; j < n; j++)
