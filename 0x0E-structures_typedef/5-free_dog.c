626717e576c3f57eb594055daefd6979589c11a2
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-14 22:15:19 +0100

    feat: wrote free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a struct dog
+ * @d: pointer to struct dog
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
