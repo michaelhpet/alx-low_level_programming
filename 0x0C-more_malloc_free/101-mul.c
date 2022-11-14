89fd36c20c54f8cf2513705bf9096988b32cd312
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-14 17:28:19 +0100

    preprocessor

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,233 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: arguments' count
+ * @argv: arguments' vector
+ * Return: argv[1] * argv[2]
+ */
+
+int main(int argc, char *argv[])
+{
+	char *num1, *num2, *res, *product, *factor;
+	unsigned int bytes, num1_len, num2_len;
+
+	num1 = argv[1], num2 = argv[2];
+
+	if (argc != 3 || not_digits(num1) || not_digits(num2))
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	num1_len = _strlen(num1), num2_len = _strlen(num2);
+	if (num1_len >= num2_len)
+	{
+		bytes = num1_len + 1;
+		product = _strcpy(product, num1);
+		factor = _strcpy(factor, num2);
+	}
+	else
+	{
+		bytes = num2_len + 1;
+		product = _strcpy(product, num2);
+		factor = _strcpy(factor, num1);
+	}
+
+	res = malloc(sizeof(*res) * bytes);
+	while (*factor != 0)
+	{
+		res = infinite_add(product, product, bytes);
+		strdecrement(factor);
+	}
+
+	printf("%s\n", res);
+
+	return (0);
+}
+
+
+/**
+ * not_digits - checks if a string contains symbols that are not digits
+ * @s: string
+ * Return: 0 if all symbols are digits, 1 otherwise
+ */
+
+int not_digits(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] < 48 || s[i] > 57)
+			return (1);
+	}
+
+	return (0);
+}
+
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string
+ * Return: length of s
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+
+	return (i);
+}
+
+
+/**
+ * strdecrement - decrements a string of numbers by 1
+ * @s: string
+ * Return: modified string
+ */
+
+char *strdecrement(char *s)
+{
+	char *res;
+	unsigned int len;
+
+	if (*s == 0)
+		return (s);
+
+	len = _strlen(s);
+	res = malloc(sizeof(*res) * (len + 1));
+	res = _strcpy(res, s);
+
+	res = itoa(atoi(res) - 1);
+
+	return (res);
+}
+
+
+/**
+ * _strcpy - copies a string
+ * @dest: address of string to copy to
+ * @src: address of string to copy from
+ * Return: adress of copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @size_r: size of r
+ * Return: pointer to r
+ */
+
+char *infinite_add(char *n1, char *n2, unsigned int size_r)
+{
+	unsigned int i, n1_len, n2_len, units, tens;
+	char *r, *tmp_n1, *tmp_n2;
+
+	n1_len = char_len(n1), n2_len = char_len(n2);
+
+	if (size_r < (n1_len + 1) || size_r < (n2_len + 1))
+		return (infinite_add(n1, n2, r, size_r + 1))
+
+	tmp_n1 = malloc(sizeof(char) * size_r);
+	tmp_n2 = malloc(sizeof(char) * size_r);
+	r = malloc(sizeof(char) * size_r);
+
+	n1 = prepend_zero(n1, tmp_n1, n1_len, size_r);
+	n2 = prepend_zero(n2, tmp_n2, n2_len, size_r);
+
+	tens = 0;
+	for (i = 1; i <= size_r; i++)
+	{
+		units = (n1[size_r - i] - 48) + (n2[size_r - i] - 48) + tens;
+		if (units > 9)
+		{
+			tens = units / 10;
+			units %= 10;
+		}
+		else
+			tens = 0;
+		r[size_r - i] = units + 48;
+	}
+	r[size_r] = 0;
+
+	r = trim_zero(r);
+	if (tens > 0 || _strlen(r) > size_r - 1)
+		return (infinite_add(n1, n2, r, size_r + 1));
+
+	return (r);
+}
+
+/**
+ * prepend_zero - prepends zeros to the beginning of a strinf
+ * @s: string
+ * @dest: pointer to write into
+ * @s_len: length of s
+ * @size: desired length of string
+ * Return: pointer to result
+ */
+
+char *prepend_zero(char *s, char *dest, int s_len, int size)
+{
+	int i, diff;
+
+	diff = size - s_len;
+	for (i = 0; i < size; i++)
+	{
+		if (i < diff)
+			dest[i] = 48;
+		else
+			dest[i] = s[i - diff];
+	}
+
+	dest[i] = 0;
+
+	return (dest);
+}
+
+/**
+ * trim_zero - removes leading zero from string
+ * @s: string to parse
+ * Return: pointer to s
+ */
+
+char *trim_zero(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] - 48 != 0)
+		{
+			s = s + i;
+			break;
+		}
+	}
+
+	return (s);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,11 @@ unsigned int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int not_digits(char *s);
+char *strdecrement(char *s);
+char *_strcpy(char *dest, char *src);
+char *prepend_zero(char *s, char *dest, int s_len, int size);
+char *trim_zero(char *s);
+char *infinite_add(char *n1, char *n2, unsigned int size_r);
 
 #endif
