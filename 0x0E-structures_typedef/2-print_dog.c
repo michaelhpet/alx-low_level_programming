dfe9f42182534ea468521766516e2fbd96541950
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-14 19:41:05 +0100

    fix: du && print_dog returns void

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,21 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name == NULL)
-			puts("Name: (nil)");
-		else
-			printf("Name: %s\n", d->name);
+	if (d == NULL)
+		return;
 
-		if (d->age < 0)
-			puts("Age: (nil)");
-		else
-			printf("Age: %f\n", d->age);
+	if (d->name == NULL)
+		puts("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
 
-		if (d->owner == NULL)
-			puts("Owner: (nil)");
-		else
-			printf("owner: %s\n", d->owner);
-	}
+	if (d->age < 0)
+		puts("Age: (nil)");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		puts("Owner: (nil)");
+	else
+		printf("owner: %s\n", d->owner);
 }
