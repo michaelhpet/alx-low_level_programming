57fe3fbcfc97a71b7c15e6e2554329c7de00b686
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-14 18:55:43 +0100

    feat: struct dog

---
+++ dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog
+ * @name: name of dog
+ * @owner: name of dog's owner
+ * @age: age of dog
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif /* _DOG_H_ */
