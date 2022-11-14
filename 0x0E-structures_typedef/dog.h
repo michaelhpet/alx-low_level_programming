91ed24822c4a3cd366a113c509b201a63f632687
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-14 20:19:30 +0100

    feat: prototype for free_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,5 +24,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
