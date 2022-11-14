de64a9bf2425ca78cbf2eabc05191faa9d2499e4
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-14 19:24:55 +0100

    chore: no report

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-
-		if (!d->age)
-			puts("Age: (nil)");
-		else
-			printf("Age: %f\n", d->age);
-
+		printf("Age: %f\n", d->age);
 		printf("owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
