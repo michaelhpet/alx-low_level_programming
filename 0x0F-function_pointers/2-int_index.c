fc76ca76233261ef434070d92af4cb8537ec560b
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-16 19:20:21 +0100

    chore: tested int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		return;
-
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i])i)
+		if (cmp(array[i]))
 			return (i);
 	}
 
