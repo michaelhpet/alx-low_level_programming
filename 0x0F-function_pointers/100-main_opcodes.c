59f96467e16ec57d2036a7d55796d95b47640afe
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-16 23:10:07 +0100

    feat: print opcodes for main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: arguments' count
+ * @argv: arguments' vector
+ * Return: 0 (success) or non-zero (error)
+*/
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	int (*self)(int, char **);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	self = &main;
+	for (i = 0; i < (bytes - 1); i++)
+	{
+		printf("%.2x", *(unsigned char *)(self + i));
+
+		if (i < (bytes - 1))
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
