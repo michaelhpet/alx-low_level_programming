18d280870bd8cadd0e1b17a06f984ba5b5a297bd
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-16 21:37:10 +0100

    feat: wrote op_functions - 5 functions for arithmetic operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - sums two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a + b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of two integers
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of two integers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of an integer by another
+ * @a: numerator
+ * @b: denominator
+ * Return: a / b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of an integer by another
+ * @a: dividend
+ * @b: divisor
+ * Return: a % b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
