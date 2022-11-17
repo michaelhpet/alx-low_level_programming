e4179b65597cf7247641c82af0cd8cd8e3c28ebb
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-17 18:03:03 +0100

    feat: wrote sum_them_all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of arguments passed
+ * Return: sum of all arguments
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i, sum;
+
+	va_start(args, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
