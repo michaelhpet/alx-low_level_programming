0fb5517d301fa4726d7b2cf294a389c4934cc0d2
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-24 10:24:19 +0100

    feat: added struct list and list_t typedef

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,24 @@
 
 #include <stdlib.h>
 
+/**
+ * struct list - a list of string structs
+ * @str: string value
+ * @len: length of str
+ * @next: next string node
+*/
+struct list
+{
+	char *str;
+	int len;
+	struct list *next;
+};
+
+/**
+ * list_t - typedef for struct list
+*/
+typedef struct list list_t;
+
 size_t print_list(const list_t *h);
 
 #endif /* _LISTS_H_ */
