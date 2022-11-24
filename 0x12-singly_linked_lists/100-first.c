4d73d5c3f7b292054957a3f41a9a2b0e554029f9
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-24 12:34:28 +0100

    feat: renamed _constructor to first and wrote first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * first - startup function that runs before main
+*/
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
