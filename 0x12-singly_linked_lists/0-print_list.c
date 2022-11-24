656d1e9f9137f08f23a1dd7fe8fdcc2394dd0170
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-24 10:28:37 +0100

    chore: tested print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list_t list
@@ -9,7 +10,8 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 
-	while (h->next)
+	count = 0;
+	while (h)
 	{
 		if (h->str)
 			printf("[%d] %s\n", h->len, h->str);
