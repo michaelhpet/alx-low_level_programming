285cdae3c58837b8f22577344a938f68076442b4
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-24 13:07:18 +0100

    fix: removed prototype of first and annotate attribute at function definition

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,9 @@
  * first - startup function that runs before main
 */
 
+__attribute__ ((constructor))
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
