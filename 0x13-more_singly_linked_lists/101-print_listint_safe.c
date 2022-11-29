f18d9c040c2d7304378d6e5bb5f9b3bc5a3a5f52
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-29 22:52:05 +0100

    refac: rewrote print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,72 +8,35 @@
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, idx;
+	size_t i, j;
+	const listint_t *head_ref, *tail_ref;
 
-	nodes = list_len(head);
+	if (head == NULL)
+		exit(98);
 
-	if (nodes == 0)
+	i = 0;
+	head_ref = head;
+	tail_ref = NULL;
+	while (head_ref)
 	{
-		while (head)
+		printf("[%p] %d\n", (void *)head_ref, head_ref->n);
+		i++;
+		head_ref = head_ref->next;
+
+		tail_ref = head;
+		j = 0;
+		while (j < i)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-			nodes++;
-		}
-	}
-	else
-	{
-		for (idx = 0; idx < nodes; idx++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-
-		printf("-> [%p] %d\n", (void *)head, head->n);
-	}
-
-	return (nodes);
-}
-
-/**
- * list_len - returns total unique nodes in a linked list
- * @head: head of list
- * Return: length of unique elements of list
-*/
-size_t list_len(const listint_t *head)
-{
-	size_t count;
-	const listint_t *fore, *hind;
-
-	count = 1;
-	hind = head->next;
-	fore = hind->next;
-
-	while (fore)
-	{
-		if (hind == fore)
-		{
-			hind = head;
-			while (hind != fore)
+			if (head_ref == tail_ref)
 			{
-				count++;
-				hind = hind->next;
-				fore = fore->next;
+				printf("-> [%p] %d\n", (void *)head_ref, head_ref->n);
+				return (i);
 			}
 
-			hind = hind->next;
-			while (hind != fore)
-			{
-				count++;
-				hind = hind->next;
-			}
-
-			return (count);
+			tail_ref = tail_ref->next;
+			j++;
 		}
-
-		hind = hind->next;
-		fore = (fore->next)->next;
 	}
 
-	return (0);
+	return (i);
 }
