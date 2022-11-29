a6055b2288b80960d6e166ed282d60b78b7636b6
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-29 23:01:20 +0100

    ref: refactored function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,35 +8,27 @@
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, j;
-	const listint_t *head_ref, *tail_ref;
+	size_t count;
+	long int ptr_diff;
 
 	if (head == NULL)
 		exit(98);
 
-	i = 0;
-	head_ref = head;
-	tail_ref = NULL;
-	while (head_ref)
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)head_ref, head_ref->n);
-		i++;
-		head_ref = head_ref->next;
+		ptr_diff = head - head->next;
+		count++;
 
-		tail_ref = head;
-		j = 0;
-		while (j < i)
+		printf("[%p] %d\n", (void *)head, head->n);
+
+		if (ptr_diff > 0)
+			head = head->next;
+		else
 		{
-			if (head_ref == tail_ref)
-			{
-				printf("-> [%p] %d\n", (void *)head_ref, head_ref->n);
-				return (i);
-			}
-
-			tail_ref = tail_ref->next;
-			j++;
+			printf("-> [%p] %d\n", (void *)head->next, (head->next)->n);
+			break;
 		}
 	}
 
-	return (i);
+	return (count);
 }
