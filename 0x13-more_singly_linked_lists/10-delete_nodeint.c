7a50c99274ff38f8e050d4f4d7f4f30e582889fe
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-29 20:20:12 +0100

    chore: tested delete_nodeint_at_index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (curr_idx = index - 1)
+	while (curr_idx < index - 1)
 	{
 		if (head_ref == NULL || head_ref->next == NULL)
 			return (-1);
