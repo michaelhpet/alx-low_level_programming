9e69d91aa6c700ae7040ef025ca42811628d4393
Michael Peter <michaelpeter.ai@gmail.com>
2022-11-29 18:13:51 +0100

    feat: wrote free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to head of list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head)
+	{
+		while(*head)
+		{
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
+		}
+	}
+
+	head = NULL;
+}
