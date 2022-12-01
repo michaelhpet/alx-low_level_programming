ada9485666a8333c9dd71005de5ee0d907a5b23e
Michael Peter <michaelpeter.ai@gmail.com>
2022-12-01 22:34:18 +0100

    feat: prototype for clear_bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int is_not_bin(const char *s);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* _MAIN_H_ */
