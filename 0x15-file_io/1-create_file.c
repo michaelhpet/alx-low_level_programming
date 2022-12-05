a31fbdaf6631dcec62812f5349039e6a681a715e
Michael Peter <michaelpeter.ai@gmail.com>
2022-12-05 21:54:54 +0100

    chore: tested create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	w_count = write_to_file(fd, text_content);
-	close(fd);
 	if (w_count == -1)
 		return (-1);
 
+	close(fd);
 	return (1);
 }
 
@@ -36,6 +36,9 @@ int write_to_file(int fd, char *s)
 	unsigned int len;
 
 	len = _strlen(s);
+	if (len == 0)
+		s = "";
+
 	return (write(fd, s, len));
 }
 
