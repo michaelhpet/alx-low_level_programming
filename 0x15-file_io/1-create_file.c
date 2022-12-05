58337fcc9e817503a380508044791961e4e7f7c0
Michael Peter <michaelpeter.ai@gmail.com>
2022-12-05 21:32:23 +0100

    chore: tested create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * create_file - creates a file
  * @filename: name of file
@@ -6,27 +8,48 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, w_count;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY);
-	if (fd)
-	{
-		while (text_content++)
-			write(fd, text_content, 1);
-
-		close(fd);
-		return (0)
-	}
-
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	while (text_content++)
-		write(fd, text_content, 1);
+	w_count = write_to_file(fd, text_content);
+	if (w_count == -1)
+		return (-1);
 
-	return (1)
+	return (1);
+}
+
+/**
+ * write_to_file - writes to a file
+ * @fd: file descriptor
+ * @s: string to write
+ * Return: write (2) return value
+*/
+int write_to_file(int fd, char *s)
+{
+	unsigned int len;
+
+	len = _strlen(s);
+	return (write(fd, s, len));
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string
+ * Return: length of s
+*/
+unsigned int _strlen(char *s)
+{
+	unsigned int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+
+	return (len);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,7 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int write_to_file(int fd, char *s);
+unsigned int _strlen(char *s);
 
 #endif /* _MAIN_H_ */
