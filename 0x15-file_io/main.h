499e95db0e0425a89c2cb2d53de433a0f3599a4e
Michael Peter <michaelpeter.ai@gmail.com>
2022-12-05 22:09:32 +0100

    feat: prototype for append_text_to_file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int write_to_file(int fd, char *s);
 unsigned int _strlen(char *s);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* _MAIN_H_ */
