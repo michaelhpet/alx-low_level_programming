0f32d7d8f3b68760677847e836362075d4d359ad
Michael Peter <michaelpeter.ai@gmail.com>
2022-12-05 22:01:51 +0100

    chore: tested create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -36,7 +36,7 @@ int write_to_file(int fd, char *s)
 	unsigned int len;
 
 	len = _strlen(s);
-	if (len == 0)
+	if (!s)
 		s = "";
 
 	return (write(fd, s, len));
