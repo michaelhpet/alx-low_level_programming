623f05b929a26b7edb37b7cb6cd1d32dd30a67bf
Michael Peter <michaelpeter.ai@gmail.com>
2022-12-05 22:29:05 +0100

    feat: wrote and tested append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,12 +8,12 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, w_count;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd)
 	{
 		if (text_content)
