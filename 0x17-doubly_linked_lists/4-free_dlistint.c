f2ae2ddafb6a54bbe383f36c5575d7685eb48a77
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-12 10:42:16 +0100

    feat: wrote free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	current = head;
+
+	while (current->next)
+	{
+		current = current->next;
+		free(current->prev);
+	}
+
+	if (current)
+		free(current);
+}
