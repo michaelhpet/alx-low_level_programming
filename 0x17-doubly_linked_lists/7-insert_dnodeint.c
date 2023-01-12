c3a602088e27e1b78b382c24ba7f2110727eb71b
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-12 20:00:36 +0100

    fix: NULL check for **h

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t  *new_node, *current = *h;
 
+	if (h == NULL)
+		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
