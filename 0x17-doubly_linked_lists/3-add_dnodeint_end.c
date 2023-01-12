cf2cabb1c402366bd079d6c399db4c544d13be2d
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-12 10:35:48 +0100

    feat: wrote add_dnodeint_end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: pointer to pointer to head node
+ * @n: integer member of new node struct
+ * Return: address of new tail[/node]
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *current;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	current = *head;
+	if (current == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (*head);
+	}
+	
+	if (current->next == NULL)
+	{
+		new_node->prev = current;
+		current->next = new_node;
+		return(*head);
+	}
+
+	while(current->next)
+		current = current->next;
+
+	new_node->prev = current;
+	current->next = new_node;
+	return(*head);
+}
