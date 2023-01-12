c802744f85d99f4dfaf4b71f08566dcff697ad3c
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-12 11:41:43 +0100

    fix: betty styles for header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /**
- * struct slistint_s - doubly linked list
+ * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
@@ -15,9 +15,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 
