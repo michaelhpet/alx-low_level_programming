ef68f04534031a5372778dc135ae43afeb4d0d0c
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-12 18:54:12 +0100

    fix: little bug in insert_dnodeint_at_index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,10 +9,10 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node,;
-
+	dlistint_t *new_node;
 	unsigned int i = 0;
 	dlistint_t  *current = *h;
+
 	if (current == NULL && idx == 0)
 	{
 		new_node = malloc(sizeof(dlistint_t));
@@ -30,7 +30,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (new_node == NULL)
 				return (NULL);
 			new_node->n = n;
-			new_node->prev = current->prev, current->prev->next = new_node;
+			new_node->prev = current->prev;
+			if (current->prev)
+				current->prev->next = new_node;
+			else
+				new_node->prev = NULL;
 			new_node->next = current, current->prev = new_node;
 			return (*h);
 		}
