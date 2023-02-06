4bfc587a524cc1b8fbc0fe64083049ee267c3f61
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 23:03:46 +0100

    feat: wrote hash_table_create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ * Return: pointer to newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long i;
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
