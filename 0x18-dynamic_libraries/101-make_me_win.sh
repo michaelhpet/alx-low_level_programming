ee2acd43868d66884fa9e4d86ac354bb357ef81b
Michael Peter <michaelhpet@gmail.com>
2023-03-23 09:46:42 +0100

    fix: LD_PRELOAD fixes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://raw.github.com/michaelhpet/alx-low_level_programming/main/0x18-dynamic_libraries/libantirand.so
-export LD_PRELOAD=/tmp/nrandom.so
+export LD_PRELOAD=/tmp/libantirand.so
