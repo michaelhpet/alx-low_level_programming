9286061288e45a4450c7be2543d71fb514f4a1c3
Michael Peter <michaelhpet@gmail.com>
2023-07-12 23:36:13 +0100

    feat: complete interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+
+/**
+* interpolation_search - Search for a value in array using interpolation search
+* @array: Array to search
+* @size: Number of elements in array
+* @value: Value to search for
+* Return: 1 if found, -1 otherwise
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i, m, n;
+
+	if (!array)
+		return (-1);
+
+	m = 0;
+	n = size - 1;
+	while(n >= m)
+	{
+		i = m + (((double)(n - m) / (array[n] - array[m])) * (value - array[m]));
+		if (i >= size)
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		
+		if (array[i] > value)
+			n = i - 1;
+		else
+			m = i + 1;
+	}
+
+	return (-1);
+}
